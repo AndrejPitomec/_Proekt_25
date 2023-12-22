6d68b23405c9a56dc45c5c338ecf533b4e96832f
AndrejPitomec <148689748+AndrejPitomec@users.noreply.github.com>
2023-12-22 09:53:46 +0100

    Add files via upload

---
+++ ProektX2/src/test.java
@@ -0,0 +1,13 @@
+
+public class test {
+	public static void main(String[] args){  
+		 
+		Stuednet s=new Stuednet();  
+		 
+		s.setName("vijay");  
+		  
+		System.out.println(s.getName());  
+		}  
+		}  
+
+
